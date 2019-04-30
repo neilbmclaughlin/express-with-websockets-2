3fa17735701e3ac696e8efc6db92814ec80d2ff7
neil mclaughlin <neil.mclaughlin4@nhs.net>
2019-04-30 13:53:05 +0100

    Add websocket example

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const http = require('http');
+const io = require('socket.io').listen(3001);
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const handler = (req, res) => {
+  fs.readFile(`${__dirname}/index.html`, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end(`Error loading ${__dirname}/index.html`);
+    } else {
+      res.writeHead(200);
+      res.end(data);
+    }
+  });
+};
+
+
+const server = http.createServer(handler);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+io.sockets.on('connection', (socket) => {
+  socket.emit('news', { news: 'hello world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
 });
